f104095f40c1ff4b445b4479450777fd28f92a5b
Emre Kaya <133666788+Excalibur312@users.noreply.github.com>
2023-11-06 02:08:18 +0300

    Add files via upload

---
+++ ikincilEkran/loginScreen.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
+
+namespace CompanyManagmentSystem
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void login_Click(object sender, EventArgs e)
+        {
+            ikincilekran ikinciEkranForm = new ikincilekran();
+            ikinciEkranForm.Show();
+
+            // Şu anki formu gizleyebilirsiniz, isteğe bağlı
+            this.Hide();
+        }
+    }
+}
