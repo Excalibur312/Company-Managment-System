52e27342b9bff6bf38ded813e3bc601b67f1fe37
Emre Kaya <133666788+Excalibur312@users.noreply.github.com>
2023-11-06 02:33:16 +0300

    Add files via upload

---
+++ ikincil ekran/ikincilekran.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ikincil_ekran
+{
+    public partial class ikincilekran : Form
+    {
+        public ikincilekran()
+        {
+            InitializeComponent();
+        }
+    }
+}
