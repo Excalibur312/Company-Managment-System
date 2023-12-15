591a0b33305da06b94ac0404d4476eeeb75b62e8
Ahmet Fevzi Can <105818318+Ahmetfcan43@users.noreply.github.com>
2023-12-15 13:45:22 +0300

    Add files via upload

---
+++ loginScreen/IseAlim.Designer.cs
@@ -0,0 +1,218 @@
+﻿namespace loginScreen
+{
+    partial class IseAlim
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBoxName = new System.Windows.Forms.TextBox();
+            this.textBoxUsername = new System.Windows.Forms.TextBox();
+            this.textBoxSurname = new System.Windows.Forms.TextBox();
+            this.textBoxPassword = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.labelUsername = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.buttonRecord = new System.Windows.Forms.Button();
+            this.comboBoxAuthority = new System.Windows.Forms.ComboBox();
+            this.comboBoxDepartment = new System.Windows.Forms.ComboBox();
+            this.comboBoxAuthorityLevel = new System.Windows.Forms.ComboBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(80, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(44, 16);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Name";
+            // 
+            // textBoxName
+            // 
+            this.textBoxName.Location = new System.Drawing.Point(83, 28);
+            this.textBoxName.Name = "textBoxName";
+            this.textBoxName.Size = new System.Drawing.Size(175, 22);
+            this.textBoxName.TabIndex = 1;
+            // 
+            // textBoxUsername
+            // 
+            this.textBoxUsername.Location = new System.Drawing.Point(83, 139);
+            this.textBoxUsername.Name = "textBoxUsername";
+            this.textBoxUsername.Size = new System.Drawing.Size(175, 22);
+            this.textBoxUsername.TabIndex = 2;
+            // 
+            // textBoxSurname
+            // 
+            this.textBoxSurname.Location = new System.Drawing.Point(83, 83);
+            this.textBoxSurname.Name = "textBoxSurname";
+            this.textBoxSurname.Size = new System.Drawing.Size(175, 22);
+            this.textBoxSurname.TabIndex = 3;
+            // 
+            // textBoxPassword
+            // 
+            this.textBoxPassword.Location = new System.Drawing.Point(83, 195);
+            this.textBoxPassword.Name = "textBoxPassword";
+            this.textBoxPassword.Size = new System.Drawing.Size(175, 22);
+            this.textBoxPassword.TabIndex = 4;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(80, 64);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(61, 16);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "Surname";
+            // 
+            // labelUsername
+            // 
+            this.labelUsername.AutoSize = true;
+            this.labelUsername.Location = new System.Drawing.Point(83, 120);
+            this.labelUsername.Name = "labelUsername";
+            this.labelUsername.Size = new System.Drawing.Size(70, 16);
+            this.labelUsername.TabIndex = 7;
+            this.labelUsername.Text = "Username";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(80, 176);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(67, 16);
+            this.label4.TabIndex = 8;
+            this.label4.Text = "Password";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(83, 286);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(58, 16);
+            this.label5.TabIndex = 9;
+            this.label5.Text = "Authority";
+            // 
+            // buttonRecord
+            // 
+            this.buttonRecord.Location = new System.Drawing.Point(359, 356);
+            this.buttonRecord.Name = "buttonRecord";
+            this.buttonRecord.Size = new System.Drawing.Size(175, 45);
+            this.buttonRecord.TabIndex = 10;
+            this.buttonRecord.Text = "Record It";
+            this.buttonRecord.UseVisualStyleBackColor = true;
+            this.buttonRecord.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // comboBoxAuthority
+            // 
+            this.comboBoxAuthority.FormattingEnabled = true;
+            this.comboBoxAuthority.Location = new System.Drawing.Point(83, 305);
+            this.comboBoxAuthority.Name = "comboBoxAuthority";
+            this.comboBoxAuthority.Size = new System.Drawing.Size(175, 24);
+            this.comboBoxAuthority.TabIndex = 11;
+            // 
+            // comboBoxDepartment
+            // 
+            this.comboBoxDepartment.FormattingEnabled = true;
+            this.comboBoxDepartment.Location = new System.Drawing.Point(83, 243);
+            this.comboBoxDepartment.Name = "comboBoxDepartment";
+            this.comboBoxDepartment.Size = new System.Drawing.Size(175, 24);
+            this.comboBoxDepartment.TabIndex = 12;
+            // 
+            // comboBoxAuthorityLevel
+            // 
+            this.comboBoxAuthorityLevel.FormattingEnabled = true;
+            this.comboBoxAuthorityLevel.Location = new System.Drawing.Point(86, 367);
+            this.comboBoxAuthorityLevel.Name = "comboBoxAuthorityLevel";
+            this.comboBoxAuthorityLevel.Size = new System.Drawing.Size(172, 24);
+            this.comboBoxAuthorityLevel.TabIndex = 13;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(83, 224);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(77, 16);
+            this.label3.TabIndex = 14;
+            this.label3.Text = "Department";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(83, 348);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(94, 16);
+            this.label6.TabIndex = 15;
+            this.label6.Text = "Authority Level";
+            // 
+            // IseAlim
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.comboBoxAuthorityLevel);
+            this.Controls.Add(this.comboBoxDepartment);
+            this.Controls.Add(this.comboBoxAuthority);
+            this.Controls.Add(this.buttonRecord);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.labelUsername);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.textBoxPassword);
+            this.Controls.Add(this.textBoxSurname);
+            this.Controls.Add(this.textBoxUsername);
+            this.Controls.Add(this.textBoxName);
+            this.Controls.Add(this.label1);
+            this.Name = "IseAlim";
+            this.Text = "IseAlim";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBoxName;
+        private System.Windows.Forms.TextBox textBoxUsername;
+        private System.Windows.Forms.TextBox textBoxSurname;
+        private System.Windows.Forms.TextBox textBoxPassword;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label labelUsername;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Button buttonRecord;
+        private System.Windows.Forms.ComboBox comboBoxAuthority;
+        private System.Windows.Forms.ComboBox comboBoxDepartment;
+        private System.Windows.Forms.ComboBox comboBoxAuthorityLevel;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label6;
+    }
+}
---
+++ loginScreen/IseAlim.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
+
+namespace loginScreen
+{
+    public partial class IseAlim : Form
+    {
+
+        public SqlConnection connection;
+        public SqlCommand command;
+        public string connectionString = "Data Source=172.16.23.125;Initial Catalog=CompanyManagment;User ID=Fevzi;Password=123;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;";
+        public IseAlim()
+        {
+            InitializeComponent();
+            connection = new SqlConnection(connectionString);
+            command = new SqlCommand();
+            command.Connection = connection;
+
+            object[] departments = new object[] { "Computer Engineer", "Robotic Engineer", "Software Engineer" };
+            comboBoxDepartment.Items.AddRange(departments);
+
+            object[] authorities = new object[] { "Manager", "Intern", "Employee" };
+            comboBoxAuthority.Items.AddRange(authorities);
+
+            object[] authoritiesLevels = new object[] { "0", "1", "2" };
+            comboBoxAuthorityLevel.Items.AddRange(authoritiesLevels);
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                try
+                {
+                    connection.Open();
+
+                    string query = "INSERT INTO UserTable (name, surname, username, password, department, authority, authoritylevel) VALUES (@name, @surname, @username, @password, @department, @authority, @authoritylevel)";
+
+                    SqlCommand command = new SqlCommand(query, connection);
+                    command.Parameters.AddWithValue("@name", textBoxName.Text);
+                    command.Parameters.AddWithValue("@surname", textBoxSurname.Text);
+                    command.Parameters.AddWithValue("@username", textBoxUsername.Text);
+                    command.Parameters.AddWithValue("@password", textBoxPassword.Text);
+                    command.Parameters.AddWithValue("@department", comboBoxDepartment.SelectedItem.ToString());
+                    command.Parameters.AddWithValue("@authority", comboBoxAuthority.SelectedItem.ToString());
+                    command.Parameters.AddWithValue("@authoritylevel", comboBoxAuthorityLevel.SelectedItem.ToString());
+
+                    int rowsAffected = command.ExecuteNonQuery();
+
+                    textBoxName.Text = " ";
+                    textBoxSurname.Text = " ";
+                    textBoxUsername.Text = " ";
+                    textBoxPassword.Text = " ";
+                    comboBoxDepartment.SelectedIndex = -1;
+                    comboBoxAuthority.SelectedIndex = -1;
+                    comboBoxAuthorityLevel.SelectedIndex = -1;
+                    
+
+                    if (rowsAffected > 0)
+                    {
+                        MessageBox.Show("Veritabanına kaydedildi.");
+                    }
+                    else
+                    {
+                        MessageBox.Show("Kaydetme işlemi başarısız oldu.");
+                    }
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("Hata: " + ex.Message);
+                }
+            }
+        }
+    }
+}
