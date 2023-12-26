2d0f0e158e478c30bc6879a2628f2921f310dc04
Ahmet Fevzi Can <105818318+Ahmetfcan43@users.noreply.github.com>
2023-12-26 12:50:43 +0300

    Add files via upload

--- loginScreen/MailleriOku.Designer.cs
+++ loginScreen/MailleriOku.Designer.cs
@@ -28,12 +28,113 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            this.cmbAlıcı = new System.Windows.Forms.ComboBox();
+            this.listView1 = new System.Windows.Forms.ListView();
+            this.cmbDepartment = new System.Windows.Forms.ComboBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.Transparent;
+            this.label1.Location = new System.Drawing.Point(99, 174);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(58, 16);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Alan Kişi";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.BackColor = System.Drawing.Color.Transparent;
+            this.label6.Location = new System.Drawing.Point(83, 144);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(74, 16);
+            this.label6.TabIndex = 5;
+            this.label6.Text = "Departman";
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.BackColor = System.Drawing.Color.Transparent;
+            this.label9.Location = new System.Drawing.Point(118, 208);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(39, 16);
+            this.label9.TabIndex = 8;
+            this.label9.Text = "İçerik";
+            // 
+            // cmbAlıcı
+            // 
+            this.cmbAlıcı.FormattingEnabled = true;
+            this.cmbAlıcı.Location = new System.Drawing.Point(163, 171);
+            this.cmbAlıcı.Name = "cmbAlıcı";
+            this.cmbAlıcı.Size = new System.Drawing.Size(159, 24);
+            this.cmbAlıcı.TabIndex = 16;
+            this.cmbAlıcı.SelectedIndexChanged += new System.EventHandler(this.cmbAlıcı_SelectedIndexChanged);
+            // 
+            // listView1
+            // 
+            this.listView1.HideSelection = false;
+            this.listView1.Location = new System.Drawing.Point(163, 208);
+            this.listView1.Name = "listView1";
+            this.listView1.Size = new System.Drawing.Size(570, 230);
+            this.listView1.TabIndex = 17;
+            this.listView1.UseCompatibleStateImageBehavior = false;
+            // 
+            // cmbDepartment
+            // 
+            this.cmbDepartment.FormattingEnabled = true;
+            this.cmbDepartment.Location = new System.Drawing.Point(163, 141);
+            this.cmbDepartment.Name = "cmbDepartment";
+            this.cmbDepartment.Size = new System.Drawing.Size(159, 24);
+            this.cmbDepartment.TabIndex = 18;
+            this.cmbDepartment.SelectedIndexChanged += new System.EventHandler(this.cmbDepartment_SelectedIndexChanged);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(5)))), ((int)(((byte)(90)))), ((int)(((byte)(120)))));
+            this.label2.ForeColor = System.Drawing.Color.White;
+            this.label2.Location = new System.Drawing.Point(597, 20);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(56, 16);
+            this.label2.TabIndex = 19;
+            this.label2.Text = "Kullanıcı";
+            // 
+            // MailleriOku
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = global::loginScreen.Properties.Resources.mini_sceen_Çalışma_Yüzeyi_1;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.cmbDepartment);
+            this.Controls.Add(this.listView1);
+            this.Controls.Add(this.cmbAlıcı);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label1);
+            this.Name = "MailleriOku";
             this.Text = "MailleriOku";
+            this.Load += new System.EventHandler(this.MailleriOku_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.ComboBox cmbAlıcı;
+        private System.Windows.Forms.ListView listView1;
+        private System.Windows.Forms.ComboBox cmbDepartment;
+        private System.Windows.Forms.Label label2;
     }
 }
--- loginScreen/MailleriOku.cs
+++ loginScreen/MailleriOku.cs
@@ -2,19 +2,323 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Data.SqlClient;
 using System.Drawing;
 using System.Linq;
+using System.Net.NetworkInformation;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement.StartPanel;
 
 namespace loginScreen
 {
     public partial class MailleriOku : Form
     {
-        public MailleriOku()
+        public string connectionString = "Data Source=192.168.56.1;Initial Catalog=CompanyManagment;User ID=ortak;Password=123;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;";
+
+        public string username;
+        private readonly int authority;
+        private readonly int department;
+        public int numberD = 0;
+
+        public MailleriOku(string username, int authority, int department)
         {
             InitializeComponent();
+            this.username = username;
+            this.authority = authority;
+            this.department = department;
+
+            cmbAlıcı.SelectedIndexChanged += cmbAlıcı_SelectedIndexChanged;
+
+        }
+
+        public void DepartmentsByUsername(string username)
+        {
+            // SQL sorgusu: Belirli kullanıcı adına göre department sütununu al
+            string query = "SELECT department FROM UserTable WHERE username = @username";
+
+            // SQL bağlantısı ve komut oluşturma
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                using (SqlCommand command = new SqlCommand(query, connection))
+                {
+                    // Parametre ekleme
+                    command.Parameters.AddWithValue("@username", username);
+
+                    try
+                    {
+                        connection.Open();
+                        SqlDataReader reader = command.ExecuteReader();
+
+                        while (reader.Read())
+                        {
+                            string department = reader["department"].ToString();
+
+                            // Department değerlerini karşılaştır
+                            if (department == "computer")
+                            {
+                                numberD = 1;
+                            }
+                            else if (department == "software")
+                            {
+                                numberD = 2;
+                            }
+                            else if (department == "robotic")
+                            {
+                                numberD = 3;
+                            }
+                            else
+                            {
+                                MessageBox.Show("Bu kullanıcı belirtilen departmanlardan birinde çalışmıyor.");
+                            }
+                        }
+                    }
+                    catch (Exception ex)
+                    {
+                        MessageBox.Show("Hata: " + ex.Message);
+                    }
+                }
+            }
+        }
+
+        public void AddDepartments()
+        {
+            // SQL sorgusu
+            string query = "SELECT department FROM UserTable"; // Tablo ve sütun adlarını değiştirmelisiniz
+
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                using (SqlCommand command = new SqlCommand(query, connection))
+                {
+                    connection.Open();
+                    SqlDataReader reader = command.ExecuteReader();
+
+                    while (reader.Read())
+                    {
+                        // ComboBox'a verileri ekleme
+                        cmbDepartment.Items.Add(reader["depatrment"].ToString()); // Sütun adını değiştirmelisiniz
+                    }
+
+                    connection.Close();
+                }
+            }
+        }
+
+        public void AddDepartmentComputer()
+        {
+
+            // SQL sorgusu - department = 'computer' olan verileri seçiyoruz
+            string query = "SELECT username FROM UserTable WHERE department = 'Computer'"; // Tablo ve sütun adlarını değiştirmelisiniz
+
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                using (SqlCommand command = new SqlCommand(query, connection))
+                {
+                    connection.Open();
+                    SqlDataReader reader = command.ExecuteReader();
+
+                    while (reader.Read())
+                    {
+                        // ComboBox'a verileri ekleme
+                        cmbAlıcı.Items.Add(reader["username"].ToString()); // Sütun adını değiştirmelisiniz
+                    }
+
+                    connection.Close();
+                }
+            }
+        }
+
+        public void AddDepartmentSoftware()
+        {
+            string query = "SELECT username FROM UserTable WHERE department = 'Software'"; // Tablo ve sütun adlarını değiştirmelisiniz
+
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                using (SqlCommand command = new SqlCommand(query, connection))
+                {
+                    connection.Open();
+                    SqlDataReader reader = command.ExecuteReader();
+
+                    while (reader.Read())
+                    {
+                        // ComboBox'a verileri ekleme
+                        cmbAlıcı.Items.Add(reader["username"].ToString()); // Sütun adını değiştirmelisiniz
+                    }
+
+                    connection.Close();
+                }
+            }
+        }
+
+        public void AddDepartmentRobotic()
+        {
+            string query = "SELECT username FROM UserTable WHERE department = 'Robotic'"; // Tablo ve sütun adlarını değiştirmelisiniz
+
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                using (SqlCommand command = new SqlCommand(query, connection))
+                {
+                    connection.Open();
+                    SqlDataReader reader = command.ExecuteReader();
+
+                    while (reader.Read())
+                    {
+                        // ComboBox'a verileri ekleme
+                        cmbAlıcı.Items.Add(reader["username"].ToString()); // Sütun adını değiştirmelisiniz
+                    }
+
+                    connection.Close();
+                }
+            }
+        }
+
+        public void ChangeComboBox() 
+        { 
+
+        }
+
+        private void MailleriOku_Load(object sender, EventArgs e)
+        {
+            switch (authority)
+            {
+                case 1:
+                    // manager
+                    AddDepartments();
+
+                    MessageBox.Show("You are Manager");
+                    break;
+
+                case 2:
+
+                    if (this.department == 1) 
+                    {
+                        if(numberD == 1)
+                        {
+                            AddDepartmentComputer();
+                            MessageBox.Show("You are Manager but access is restricted");
+                        }
+                        else if (numberD == 2)
+                        {
+                            AddDepartmentSoftware();
+                            MessageBox.Show("You are Manager but access is restricted");
+                        }
+                        else
+                        {
+                            AddDepartmentRobotic();
+                            MessageBox.Show("You are Manager but access is restricted");
+                        }
+                    }
+                    else
+                    {
+                        MessageBox.Show("Your level is very low (inside)");
+                    }
+                    break;
+
+                case 3:
+
+                    MessageBox.Show("Your level is very low");
+
+                    break;
+
+                default:
+
+                    AddDepartmentComputer();
+                    AddDepartmentRobotic();
+                    AddDepartmentSoftware();
+
+                    MessageBox.Show("Switch-Case Default");
+
+                    break;
+            }
+
+        }
+
+        private void cmbDepartment_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            string selectedDepartment = cmbDepartment.SelectedItem.ToString();
+
+            // İkinci ComboBox'taki öğeleri temizleyelim
+            cmbAlıcı.Items.Clear();
+
+            // Seçilen departmana göre pozisyonları ekleyelim
+            if (selectedDepartment == "Computer")
+            {
+                AddDepartmentComputer();
+            }
+            else if (selectedDepartment == "Software")
+            {
+                AddDepartmentSoftware();
+            }
+            else if (selectedDepartment == "Robotic")
+            {
+                AddDepartmentRobotic();
+            }
+            // Diğer departmanlar için gerekli durumlar buraya eklenebilir
+        }
+
+        private void cmbAlıcı_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            string selectedItem = cmbAlıcı.SelectedItem.ToString();
+
+            // ListView'i temizleme
+            listViewEmployees.Items.Clear();
+
+            // Seçili departmana göre ListView'i güncelleme
+            foreach (ListViewItem item in listViewEmployees.Items)
+            {
+                if (item.SubItems[2].Text == selectedItem)
+                {
+                    listViewEmployees.Items.Add((ListViewItem)item.Clone());
+                }
+            }
         }
     }
 }
+
+
+/*
+ 
+private void FetchEmailsAndMessages(string username)
+{
+    try
+    {
+        using (SqlConnection sqlConnection = new SqlConnection("Your_Connection_String"))
+        {
+            sqlConnection.Open();
+            string query = "SELECT Email, Message FROM YourTableName WHERE Username = @Username";
+            
+            SqlCommand cmd = new SqlCommand(query, sqlConnection);
+            cmd.Parameters.AddWithValue("@Username", username);
+
+            SqlDataReader dr = cmd.ExecuteReader();
+            while (dr.Read())
+            {
+                ListViewItem item = new ListViewItem(dr["Email"].ToString());
+                item.SubItems.Add(dr["Message"].ToString());
+                listView1.Items.Add(item);
+            }
+        }
+    }
+    catch (Exception ex)
+    {
+        MessageBox.Show(ex.Message);
+    }
+}
+
+/////
+///////
+///
+
+private void YourButton_Click(object sender, EventArgs e)
+{
+    string username = "desired_username"; // İlgili kullanıcı adını burada belirtin
+
+    // Belirtilen kullanıcı adına göre e-posta ve mesajları getir
+    FetchEmailsAndMessages(username);
+}
+
+*/
+
+
+
