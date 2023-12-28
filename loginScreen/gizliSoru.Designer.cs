6f71788e8484d622a244408211e703630d330cf3
halenursarisen <77547665+halenursarisen@users.noreply.github.com>
2023-12-28 23:19:13 +0300

    Add files via upload

---
+++ loginScreen/gizliSoru.Designer.cs
@@ -0,0 +1,143 @@
+﻿namespace loginScreen
+{
+    partial class gizliSoru
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblGizliSoruC = new System.Windows.Forms.Label();
+            this.BtnGizliBlrl = new loginScreen.CustomBtn();
+            this.BtnGeriG = new loginScreen.CustomBtn();
+            this.txtCevapBelirle = new System.Windows.Forms.TextBox();
+            this.lblKullanici = new System.Windows.Forms.Label();
+            this.txtKullanici = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // lblGizliSoruC
+            // 
+            this.lblGizliSoruC.AutoSize = true;
+            this.lblGizliSoruC.BackColor = System.Drawing.Color.Transparent;
+            this.lblGizliSoruC.Location = new System.Drawing.Point(308, 141);
+            this.lblGizliSoruC.Name = "lblGizliSoruC";
+            this.lblGizliSoruC.Size = new System.Drawing.Size(164, 16);
+            this.lblGizliSoruC.TabIndex = 0;
+            this.lblGizliSoruC.Text = "Gizli Cevabınızı Belirleyiniz";
+            // 
+            // BtnGizliBlrl
+            // 
+            this.BtnGizliBlrl.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(5)))), ((int)(((byte)(90)))), ((int)(((byte)(120)))));
+            this.BtnGizliBlrl.BackgroundColor = System.Drawing.Color.FromArgb(((int)(((byte)(5)))), ((int)(((byte)(90)))), ((int)(((byte)(120)))));
+            this.BtnGizliBlrl.BorderCoIor = System.Drawing.Color.PaleVioletRed;
+            this.BtnGizliBlrl.BorderRadius = 20;
+            this.BtnGizliBlrl.BorderSize = 0;
+            this.BtnGizliBlrl.FlatAppearance.BorderSize = 0;
+            this.BtnGizliBlrl.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.BtnGizliBlrl.ForeColor = System.Drawing.Color.White;
+            this.BtnGizliBlrl.Location = new System.Drawing.Point(327, 241);
+            this.BtnGizliBlrl.Name = "BtnGizliBlrl";
+            this.BtnGizliBlrl.Size = new System.Drawing.Size(119, 29);
+            this.BtnGizliBlrl.TabIndex = 1;
+            this.BtnGizliBlrl.Text = "Belirle";
+            this.BtnGizliBlrl.TextdColor = System.Drawing.Color.White;
+            this.BtnGizliBlrl.UseVisualStyleBackColor = false;
+            this.BtnGizliBlrl.Click += new System.EventHandler(this.BtnGizliBlrl_Click);
+            // 
+            // BtnGeriG
+            // 
+            this.BtnGeriG.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(5)))), ((int)(((byte)(90)))), ((int)(((byte)(120)))));
+            this.BtnGeriG.BackgroundColor = System.Drawing.Color.FromArgb(((int)(((byte)(5)))), ((int)(((byte)(90)))), ((int)(((byte)(120)))));
+            this.BtnGeriG.BorderCoIor = System.Drawing.Color.PaleVioletRed;
+            this.BtnGeriG.BorderRadius = 30;
+            this.BtnGeriG.BorderSize = 0;
+            this.BtnGeriG.FlatAppearance.BorderSize = 0;
+            this.BtnGeriG.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.BtnGeriG.ForeColor = System.Drawing.Color.White;
+            this.BtnGeriG.Location = new System.Drawing.Point(643, 406);
+            this.BtnGeriG.Name = "BtnGeriG";
+            this.BtnGeriG.Size = new System.Drawing.Size(128, 32);
+            this.BtnGeriG.TabIndex = 2;
+            this.BtnGeriG.Text = "Geri";
+            this.BtnGeriG.TextdColor = System.Drawing.Color.White;
+            this.BtnGeriG.UseVisualStyleBackColor = false;
+            this.BtnGeriG.Click += new System.EventHandler(this.BtnGeriG_Click);
+            // 
+            // txtCevapBelirle
+            // 
+            this.txtCevapBelirle.Location = new System.Drawing.Point(311, 182);
+            this.txtCevapBelirle.Name = "txtCevapBelirle";
+            this.txtCevapBelirle.Size = new System.Drawing.Size(157, 22);
+            this.txtCevapBelirle.TabIndex = 3;
+            // 
+            // lblKullanici
+            // 
+            this.lblKullanici.AutoSize = true;
+            this.lblKullanici.BackColor = System.Drawing.Color.Transparent;
+            this.lblKullanici.Location = new System.Drawing.Point(38, 29);
+            this.lblKullanici.Name = "lblKullanici";
+            this.lblKullanici.Size = new System.Drawing.Size(56, 16);
+            this.lblKullanici.TabIndex = 4;
+            this.lblKullanici.Text = "Kullanıcı";
+            // 
+            // txtKullanici
+            // 
+            this.txtKullanici.Location = new System.Drawing.Point(100, 23);
+            this.txtKullanici.Name = "txtKullanici";
+            this.txtKullanici.Size = new System.Drawing.Size(100, 22);
+            this.txtKullanici.TabIndex = 5;
+            // 
+            // gizliSoru
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = global::loginScreen.Properties.Resources.mini_sceen_Çalışma_Yüzeyi_1;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.txtKullanici);
+            this.Controls.Add(this.lblKullanici);
+            this.Controls.Add(this.txtCevapBelirle);
+            this.Controls.Add(this.BtnGeriG);
+            this.Controls.Add(this.BtnGizliBlrl);
+            this.Controls.Add(this.lblGizliSoruC);
+            this.Cursor = System.Windows.Forms.Cursors.Default;
+            this.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Name = "gizliSoru";
+            this.Text = "gizliSoru";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblGizliSoruC;
+        private CustomBtn BtnGizliBlrl;
+        private CustomBtn BtnGeriG;
+        private System.Windows.Forms.TextBox txtCevapBelirle;
+        private System.Windows.Forms.Label lblKullanici;
+        private System.Windows.Forms.TextBox txtKullanici;
+    }
+}
---
+++ loginScreen/gizliSoru.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace loginScreen
+{
+    public partial class gizliSoru : Form
+
+    {
+        private readonly string username;
+        public string connectionString = "Data Source=25.61.180.90;Initial Catalog=CompanyManagment;User ID=ortak;Password=123;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;";
+
+        public gizliSoru(string username)
+        {
+            InitializeComponent();
+            this.username = username;
+            txtKullanici.ReadOnly = true;   
+            txtKullanici.Text = username;
+        }
+     
+        private void BtnGizliBlrl_Click(object sender, EventArgs e)
+        {
+            string gizliCevap = txtCevapBelirle.Text;
+
+            // Kullanıcıyı GizliSoru tablosunda kontrol et
+            try
+            {
+                using (SqlConnection connection = new SqlConnection(connectionString))
+                {
+                    connection.Open();
+
+                    string findUserQuery = "SELECT COUNT(*) FROM GizliSoru WHERE username = @username";
+                    using (SqlCommand findUserCommand = new SqlCommand(findUserQuery, connection))
+                    {
+                        findUserCommand.Parameters.AddWithValue("@username", username);
+
+                        int userCount = (int)findUserCommand.ExecuteScalar();
+                        if (userCount > 0)
+                        {
+                            // Kullanıcı bulundu, cevabı güncelle
+                            string updateQuery = "UPDATE GizliSoru SET cevap = @cevap WHERE username = @username";
+                            using (SqlCommand updateCommand = new SqlCommand(updateQuery, connection))
+                            {
+                                updateCommand.Parameters.AddWithValue("@cevap", gizliCevap);
+                                updateCommand.Parameters.AddWithValue("@username", username);
+
+                                int rowsAffected = updateCommand.ExecuteNonQuery();
+                                if (rowsAffected > 0)
+                                {
+                                    MessageBox.Show("Gizli cevap başarıyla güncellendi!");
+                                }
+                                else
+                                {
+                                    MessageBox.Show("Gizli cevap güncellenirken bir hata oluştu.");
+                                }
+                            }
+                        }
+                        else
+                        {
+                            // Kullanıcı bulunamadı, yeni kayıt ekle
+                            string insertQuery = "INSERT INTO GizliSoru (username, cevap) VALUES (@username, @cevap)";
+                            using (SqlCommand insertCommand = new SqlCommand(insertQuery, connection))
+                            {
+                                insertCommand.Parameters.AddWithValue("@username", username);
+                                insertCommand.Parameters.AddWithValue("@cevap", gizliCevap);
+
+                                int rowsAffected = insertCommand.ExecuteNonQuery();
+                                if (rowsAffected > 0)
+                                {
+                                    MessageBox.Show("Yeni kullanıcı ve gizli cevap başarıyla eklendi!");
+                                }
+                                else
+                                {
+                                    MessageBox.Show("Yeni kullanıcı ve gizli cevap eklenirken bir hata oluştu.");
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Hata: " + ex.Message);
+            }
+        }
+
+        private void BtnGeriG_Click(object sender, EventArgs e)
+        {
+            this.Hide();
+        }
+    }
+}
+
